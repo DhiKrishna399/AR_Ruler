27411dcc13aad2ee940bd84ff2f641a57fb6df1f
Dhiva Krishna <dhivakrishna@gmail.com>
2020-04-23 21:26:17 -0700

    Scene Detection

--- AR_Ruler/ViewController.swift
+++ AR_Ruler/ViewController.swift
@@ -14,20 +14,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    //Array to keep track of all the dotNodes
+    var dotNodesArr = [SCNNode]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        //Visually present the points when scanning for a surface
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,29 +44,44 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    //Keeps track of touches occuring on the application
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        //touches is the set of UI touches we get when the screen is touched
+        if let touchLocation = touches.first?.location(in: sceneView){
+            //Feature point gives us the 3D location of a continuous function if it exists
+            let hitTestresults = sceneView.hitTest(touchLocation, types: .featurePoint)
+            
+            if let hitResult = hitTestresults.first{
+                addDot(at: hitResult) //Add a dot at the hit locaiton
+            }
+        }
     }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    //The external parameter
+    func addDot(at hitResult: ARHitTestResult){
+        
+    
+        let material = SCNMaterial()
+        let nodeGeo = SCNSphere(radius: 0.005)
+        material.diffuse.contents = UIColor.red
+        nodeGeo.materials = [material]
+        
+        let dotNode = SCNNode(geometry: nodeGeo)
+        
+        dotNode.position = SCNVector3(x:hitResult.worldTransform.columns.3.x, y:hitResult.worldTransform.columns.3.y, z:hitResult.worldTransform.columns.3.z)
+        sceneView.scene.rootNode.addChildNode(dotNode)
+        
+        dotNodesArr.append(dotNode)
+        
+        if dotNodesArr.count >= 2 {
+            calculate()
+        }
         
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+    func calculate(){
+        let start
     }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+ 
 }
